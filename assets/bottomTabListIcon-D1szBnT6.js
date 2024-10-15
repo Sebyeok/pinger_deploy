cfcede910e5d1275d60533e59ce8fd5c1effa44d
Sebyeok <tngnu02@gmail.com>
2024-10-16 00:27:12 +0900

    deploy

--- assets/bottomTabListIcon-D1szBnT6.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-CaujMdbl.js";const r=e=>t.createElement("svg",{viewBox:"0 0 23 20",fill:"none",xmlns:"http://www.w3.org/2000/svg",...e},t.createElement("path",{d:"M22 1L1 1.00004M22 10H1M22 19H1",stroke:"current",strokeWidth:1.5,strokeLinecap:"round"}));export{r as default};
